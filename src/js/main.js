2e1b17315c121ed77de67e791ea740b4dae1facb
Kristiāna Belova <A230250KB@rvt.lv>
2025-02-23 23:20:33 +0200

    'update'

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,47 @@
-const submit = document.querySelector('input.button')
-submit.addEventListener('click', e =>{
-  const form = document.forms['form']
-  const required = form.querySelectorAll('input.required')
-  required.forEach((ele, i) => {
-    if(required[i].value === ''){
-      const errorMassege = required[i].previousSibling.previousSibling
-      if(errorMassege){
-        errorMassege.style.display = 'block'
-        return false
-      }else{
-        errorMassege.style.display = 'none'
-      }
-      e.preventDefault
-    }
-    return true
-  })
+fetch('http://89.250.69.87/api/fact').then(async response => {
+  if(!response.ok) {
+    alert('Failed to fetch data')
+    return 0
+  }
+  const data = await response.json()
+  document.getElementById('qoute').innerHTML = data.fact
 })
 
-const catFact = document.getElementById('catfact')
-console.log(catFact.innerHTML)
+// Navigations
+const navigations = document.querySelectorAll('nav > div.link')
+const sections = document.querySelectorAll('section, .bg-image')
+navigations[0].querySelector(".romb").classList.add('active')
+const main = document.querySelector('.container')
 
-  function catFactFetch() {
-    fetch('https://catfact.ninja/fact')
-      .then((response) => response.json())
-        .then((json) => {
-          console.log(json.fact)
-          catFact.innerHTML = JSON.stringify(json.fact)
+main.addEventListener('scroll', () => {
+    let arr = []
+    sections.forEach((section, index) => {
+        var rect = section.getBoundingClientRect();
+        navigations[index].querySelector(".romb").classList.remove('active')
+        if (section.querySelector('h2') && section.querySelector('h2').getBoundingClientRect().bottom <= window.innerHeight || rect.bottom <= window.innerHeight) {
+          arr.push(index)
+        }
     })
-  }
+    navigations[arr[arr.length - 1]].querySelector(".romb").classList.add('active')
+})
 
-catFactFetch()
+//Validate email
+const submit = document.getElementById('submit')
+let text = document.getElementById('email-validation')
+const email = document.getElementById('email')
+const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+
+submit.addEventListener('click', e =>{
+  if(email.value === ''){
+    text.innerHTML = 'Please put in a valid e-mail!'
+    text.style.display = 'block'
+  }else if(!emailPattern.test(email.value)){
+    text.innerHTML = 'Please put in a valid e-mail!'
+    text.style.display = 'block'
+  }
+  else{
+    text.innerHTML = ''
+    text.style.display = 'none'
+    email.value = ''
+  }
+})
